ed1b8dbb437cb97e516f0f064dd64d787a7753fb
Jeremy Clarke <jeremy@savetrees.co.uk>
2019-02-14 11:02:53 +0000

    Redesigned to start/stop on prop value properly

--- src/component/VueSubmit.vue
+++ src/component/VueSubmit.vue
@@ -1,7 +1,7 @@
 <template lang='pug'>
 button(:id='id' class='vue-submit' :type='type' :disabled='locked' @click='click')
   slot Submit
-  span.spinner(v-if='this.started_')
+  span.spinner(v-if='this.running')
 </template>
 
 <script>
@@ -11,16 +11,10 @@ import { defaultButtonType } from './utils'
 export default {
   name: 'vue-submit',
   props: {
-    id: {
-      type: String,
-      required: true
-    },
+    id: { type: String, required: true },
     disabled: Boolean,
-    started: Boolean,
-    startOnClick: {
-      type: Boolean,
-      default: true
-    },
+    running: Boolean,
+    startOnClick: { type: Boolean, default: true },
     type: {
       type: String,
       default: function() {
@@ -28,14 +22,9 @@ export default {
       }
     }
   },
-  data() {
-    return {
-      started_: this.started
-    }
-  },
   computed: {
     locked() {
-      const locked = !!(this.started_ | !this.enabled)
+      const locked = !!(this.running | !this.enabled)
       return locked
     },
     enabled() {
@@ -44,36 +33,19 @@ export default {
     }
   },
   watch: {
-    started(newValue) {
+    running(newValue, oldValue) {
       //console.log('watch: ' + newValue)
-      if (newValue) {
-        if (!this.started_) {
-          this.start()
-        }
-      } else {
-        if (this.started_) {
-          this.stop()
-        }
+      if (newValue && oldValue === false) {
+        this.$emit('start', { id: this.id })
+      } else if (!newValue && oldValue === true) {
+        this.$emit('stop', { id: this.id })
       }
     }
   },
   methods: {
     click(event) {
-      if (this.startOnClick && !this.started_) {
-        this.start()
-      }
-      //console.log('click s: ' + this.started + ', click s_: ' + this.started_)
+      //console.log('click s: ' + this.running + ', click s_: ' + this.running_)
       this.$emit('click', event)
-    },
-    start() {
-      this.started_ = true
-      //console.log('start: ' + this.id)
-      this.$emit('start', { id: this.id })
-    },
-    stop() {
-      this.started_ = false
-      //console.log('stop: ' + this.id)
-      this.$emit('stop', { id: this.id })
     }
   }
 }
